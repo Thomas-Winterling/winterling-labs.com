30bacf9a80ff9395edbca00ba239378098aa012d
Thomas Winterling <winterlinglabs@gmail.com>
2020-10-29 17:06:14 +0100

    Create new file

---
+++ src/firebase.js
@@ -0,0 +1,20 @@
+import firebase from "firebase";
+import data from './firebase.js';
+
+const firebaseConfig = {
+    apiKey: data[1].apiKey,
+    authDomain: data[1].authDomain,
+    databaseURL: data[1].databaseURL,
+    projectId: data[1].projectId,
+    storageBucket: data[1].storageBucket,
+    messagingSenderId: data[1].messagingSenderId,
+    appId: data[1].appId,
+    measurementId: data[1].measurementId
+  };
+  
+   
+  const firebaseApp = firebase.initializeApp(firebaseConfig);
+
+  const db = firebaseApp.firestore();
+  
+  export default db;
