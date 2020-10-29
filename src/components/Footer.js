4cc836b960d176a5782aa0de0f445eabc6de55ce
Thomas Winterling <winterlinglabs@gmail.com>
2020-10-29 15:50:31 +0100

    Create Impressum

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,27 +1,23 @@
 import React from 'react';
 import Cookie from '../components/Cookie';
+import text from '../fixtures/impressum.js';
 
 
 
 function Footer() {
-
-    
-
+   
         const date = () => {
             let dateTime = new Date();
             let year = dateTime.getFullYear();
             return year;
         }
 
-   
-   
-
-
     return (
         <div className="footer">
             <p>Copyright { date() }</p>
             <p>Winterling-labs.com</p>
             <Cookie />
+            <h1>{text}</h1>
         </div>
     )
 }
