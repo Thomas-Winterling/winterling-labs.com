e8338b8936f48dba8243f0a4192acb87d0f8274c
Thomas Winterling <winterlinglabs@gmail.com>
2020-11-07 14:14:23 +0100

    Style header and footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,5 @@
 import React from 'react';
 import Cookie from '../components/Cookie';
-import Impressum from '../components/Impressum'
-import DataProtection from '../components/DataProtection'
 import { Nav } from 'react-bootstrap';
 
 
@@ -26,7 +24,7 @@ function Footer() {
                     <Nav.Link href="/impressum">Impressum</Nav.Link>
                 </Nav.Item>
                 <Nav.Item>
-                    <Nav.Link href="datenschutz">Datenschutz</Nav.Link>
+                    <Nav.Link href="/datenschutz">Datenschutz</Nav.Link>
                 </Nav.Item>
             </Nav>
             <Cookie />    
